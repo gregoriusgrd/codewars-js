f3c1ff7543fba3d8f0359298d00ce9d4e762acb2
Gregorius G <gregorius.gerald21@gmail.com>
2025-12-03 22:53:01 +0700

    3 des

---
+++ 8-kyu/strings/are-you-playing-banjo.js
@@ -0,0 +1,28 @@
+/*
+https://www.codewars.com/kata/53af2b8861023f1d88000832/train/javascript
+
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings.
+*/
+
+function areYouPlayingBanjo(name) {
+  let first = name[0]
+  let match = /[r]/i.test(first)
+  return match ? `${name} plays banjo` : `${name} does not play banjo`
+}
+
+console.log(areYouPlayingBanjo('Adam'))
+
+// alt
+
+/*
+function areYouPlayingBanjo(name) {
+  return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+}
+*/
